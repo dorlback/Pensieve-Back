cf6fe6a85ee6635fcfb6d70f659f9326647d4b17
백승헌 <bsh7829@gmail.com>
2024-06-28 02:01:21 +0900

    package add

--- manage.py
+++ manage.py
@@ -2,11 +2,15 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from dotenv import load_dotenv
 
+# API KEY 정보로드
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+    load_dotenv()
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
